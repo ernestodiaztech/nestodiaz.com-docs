071f230c933c6663d5756d8d7f8c9c0780434af3
Ernesto Diaz <139026438+ernestodiaztech@users.noreply.github.com>
2025-09-20 13:16:35 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,7 +147,7 @@ const config = {
       },
       prism: {
         theme: prismThemes.github,
-        darkTheme: prismThemes.duotoneDark,
+        darkTheme: prismThemes.dracula,
       },
     }),
 };
